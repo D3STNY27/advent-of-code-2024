1f3b55b6cad3cca8c285b3b94f4590247baf91ae
Himanshu <hmusicofficial27@gmail.com>
2024-12-23 08:27:51 +0530

    (2024) Day-22 Some Optimizations

--- day-22/part-1.py
+++ day-22/part-1.py
@@ -1,12 +1,16 @@
-
+import os
 from functools import cache
+
+
+os.system('cls')
+
 
 def read_input_file(file_path: str) -> list[str]:
   with open(file=file_path, mode="r") as input_file:
       lines = input_file.readlines()
       return [line.strip() for line in lines]
 
-@cache
+
 def mix_and_prune_secret_number(result: int, secret_number: int):
     return (secret_number ^ result) % 16777216
 
@@ -37,5 +41,4 @@ def solution(lines: list[str]):
 
 lines = read_input_file(file_path="input.txt")
 solution(lines)
-print(mix_and_prune_secret_number.cache_info())
 print(get_next_secret_number.cache_info())
--- day-22/part-2.py
+++ day-22/part-2.py
@@ -2,7 +2,9 @@ import os
 from collections import deque
 from functools import cache
 
+
 os.system('cls')
+
 
 def read_input_file(file_path: str) -> list[str]:
   with open(file=file_path, mode="r") as input_file:
@@ -10,7 +12,6 @@ def read_input_file(file_path: str) -> list[str]:
       return [line.strip() for line in lines]
 
 
-@cache
 def mix_and_prune_secret_number(result: int, secret_number: int):
     return (secret_number ^ result) % 16777216
 
@@ -23,7 +24,7 @@ def get_next_secret_number(secret_number: int):
     secret_number = mix_and_prune_secret_number(secret_number, result)
     result = secret_number * 2048
     secret_number = mix_and_prune_secret_number(secret_number, result)
-    return secret_number
+    return secret_number, int(str(secret_number)[-1])
 
 
 def solution(lines: list[str]):
@@ -31,39 +32,36 @@ def solution(lines: list[str]):
 
     for line in lines:
         current_secret_number = int(line)
-        previous_price = line[-1]
+        previous_price = int(line[-1])
         difference_sequence = deque()
 
         for _ in range(2000):
-            next_secret_number = get_next_secret_number(current_secret_number)
-            price = str(next_secret_number)[-1]
+            next_secret_number, price = get_next_secret_number(current_secret_number)
             current_secret_number = next_secret_number
             
-            difference = (int(price) - int(previous_price))
+            difference = (price - previous_price)
             
             if len(difference_sequence)==4:
                 difference_sequence.popleft()
                 difference_sequence.append(difference)
 
-                if tuple(difference_sequence) not in buying_options:
-                    buying_options[tuple(difference_sequence)] = {}
-                
-                if line not in buying_options[tuple(difference_sequence)]:
-                    buying_options[tuple(difference_sequence)][line] = []
+                difference_tuple = tuple(difference_sequence)
 
-                buying_options[tuple(difference_sequence)][line].append(int(price))
+                if difference_tuple not in buying_options:
+                    buying_options[difference_tuple] = {}
+                
+                if line not in buying_options[difference_tuple]:
+                    buying_options[difference_tuple][line] = price
             else:
                 difference_sequence.append(difference)
 
             previous_price = price
     
+
     # Calculate Max Bananas
     max_sum = float('-inf')
     for buyers in buying_options.values():
-        total_sum = 0
-        for prices in buyers.values():
-            if prices:
-                total_sum += prices[0]
+        total_sum = sum(buyers.values())
         
         if total_sum > max_sum:
             max_sum = total_sum
@@ -71,7 +69,6 @@ def solution(lines: list[str]):
     print(max_sum)
 
 
-lines = read_input_file(file_path="input.txt")
+lines = read_input_file(file_path="sample_input_2.txt")
 solution(lines)
-print(mix_and_prune_secret_number.cache_info())
 print(get_next_secret_number.cache_info())
